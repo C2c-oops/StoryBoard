d2ff5016057bc13cf8c2a70e507d9c57ab1a7e69
Sahil Singh <singhsittu430@gmail.com>
2020-04-10 19:30:42 +0530

    Added Login SignUp.

---
+++ Projects/app/src/main/java/com/c2/studentmanagementsystem/MainActivity.java
@@ -0,0 +1,64 @@
+package com.c2.studentmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1, button2, button3, button4, button5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.buttonAdd);
+        button2 = findViewById(R.id.buttonSearch);
+        button3 = findViewById(R.id.buttonView);
+        button4 = findViewById(R.id.buttonDelete);
+        button5 = findViewById(R.id.buttonUpdate);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DeleteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UpdateActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

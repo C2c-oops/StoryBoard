aa75d6a8bef90afbe1b8e5bd591d89209a344ac4
Sahil Singh <singhsittu430@gmail.com>
2021-08-08 02:53:33 +0530

    :sparkles: Added toolbar & styles

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent{
 
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakPointObserver.observe([Breakpoints.Handset])
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+  constructor(private breakPointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
   }

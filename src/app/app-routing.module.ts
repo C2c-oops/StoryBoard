28a03baaa10f44d004c91b0a52f1901e07f00bab
Sahil Singh <singhsittu430@gmail.com>
2021-08-08 13:53:21 +0530

    :sparkles: Added Auth guard & SnackBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HompPageComponent } from './homp-page/homp-page.component';
+import { AuthGuard } from './user/auth.guard';
 
 const routes: Routes = [
   {
-    path : '', component : HompPageComponent
+    path : '', 
+    component : HompPageComponent
   },
   {
-    path : 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    path : 'login', 
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  },
+  {
+    path : 'kanban', 
+    loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule),
+    canActivate: [AuthGuard]
   }
 ];
 
